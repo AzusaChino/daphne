b36b34f9631c82634ca98ebaa9cca10d9e427823
azusachino <azusachino@duck.com>
2023-05-18 20:28:01 +0800

    chore: basic go files

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello daphne")
+	app := gin.Default()
+
+	app.Use(func(ctx *gin.Context) {
+		fmt.Printf("url is %s", ctx.Request.URL)
+		ctx.Next()
+	})
+
+	app.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+    
+	app.Run()
 }
---
+++ router/v1/api.go
@@ -0,0 +1,7 @@
+package v1
+
+import "github.com/gin-gonic/gin"
+
+func ApiV1(ctx * gin.Context) {
+    
+}
